61978d38abd658a1b28378bbc42b8d6edbc1a54d
raj.rajeev15@gmail.com <raj.rajeev15@gmail.com>
2019-07-22 22:03:28 +0530

    componene Demo completed

--- ComponentsDemo/src/app/failure/failure.component.ts
+++ ComponentsDemo/src/app/failure/failure.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-failure',
   templateUrl: './failure.component.html',
-  styleUrls: ['./failure.component.css']
+  styles: [` h1 {
+      color:red;
+
+  }`]
 })
 export class FailureComponent implements OnInit {
 
